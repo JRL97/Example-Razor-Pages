47e4dd2e8c31b102b8fa0f331a646ebfc4a1a1ab
JRL97 <91621546+JRL97@users.noreply.github.com>
2022-10-06 10:18:22 +0100

    Add project files.

---
+++ Example Razor Pages/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Example_Razor_Pages.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Fullname { get; set; }
+        public string Email { get; set; }
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
